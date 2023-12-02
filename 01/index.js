e2ff65aa21a724797ac39ef3c04204e14290b956
Michal Opielka <michal.opielka@finder.com>
2023-12-02 19:29:46 +0100

    feat: 01 plus

--- 01/index.js
+++ 01/index.js
@@ -1,6 +1,26 @@
 const fs = require('node:fs');
 
-const input = fs.readFileSync('./input.txt').toString('utf8');
+const rawInput = fs.readFileSync('./input.txt').toString('utf8');
+let input = rawInput;
+
+if (process.argv[2] === 'plus') {
+  const digitNames = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+
+  for (let i = 0; i < digitNames.length; i++) {
+    input = input.replaceAll(digitNames[i], `${digitNames[i]}${i}${digitNames[i]}`);
+  }
+}
 
 console.log(
   input.split('\n')
